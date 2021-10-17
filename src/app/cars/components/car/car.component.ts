070152637ef8693686c0ccc8271c6e7d1e798280
Toyin Etiko <toyin.etiko@bolero.net>
2021-10-17 15:47:33 +0100

    added car form and some simple logs to see form working

--- src/app/cars/components/car/car.component.ts
+++ src/app/cars/components/car/car.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Subject} from 'rxjs';
+import {ActivatedRoute, Params} from '@angular/router';
+import {takeUntil} from 'rxjs/operators';
+
+import {CarService} from '../../services/car.service';
 
 @Component({
   selector: 'app-car',
@@ -7,9 +13,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  editMode = false;
+  carForm: FormGroup;
+  functionName: string;
+
+  private destroy$ = new Subject();
+
+  constructor(private route: ActivatedRoute,
+              private carService: CarService) {
+  }
 
   ngOnInit(): void {
+    this.route.params.pipe(takeUntil(this.destroy$))
+    .subscribe((params: Params) => {
+      this.id = params.id;
+      this.functionName = params.functionName;
+      this.editMode = params.id != null;
+      this.initForm();
+    });
+  }
+
+  private initForm(): void {
+    let make = '';
+    let model = '';
+    let colour = '';
+    let year;
+    let id = '';
+
+    if (this.editMode) {
+      const car = this.carService.getCar(this.id);
+
+      make = car.make;
+      model = car.model;
+      colour = car.colour;
+      year = car.year;
+      id = car.id;
+    }
+
+    this.carForm = new FormGroup({
+      id: new FormControl(id),
+      make: new FormControl(make),
+      model: new FormControl(model),
+      colour: new FormControl(colour),
+      year: new FormControl(year)
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.carForm.value);
   }
 
 }
--- src/app/cars/services/car.service.ts
+++ src/app/cars/services/car.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {LocalStorageService} from 'ngx-webstorage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  constructor() { }
+  carList: any;
+
+  constructor(private storage: LocalStorageService) {
+  }
+
+  getCars() {
+    console.log('cars');
+  }
+
+  getCar(id) {
+    console.log(id);
+  }
 }
