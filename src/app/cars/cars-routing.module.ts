7f149d37b55776d69a79f4b019ad343717bf62b7
Toyin Etiko <toyin.etiko@bolero.net>
2021-10-17 15:26:54 +0100

    updated routing

--- src/app/cars/cars-routing.module.ts
+++ src/app/cars/cars-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
+import {CarComponent} from './components/car/car.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cars', component: LandingPageComponent},
+  {path: 'cars/:functionName', component: CarComponent},
+  {path: 'cars/:functionName/:id', component: CarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CarsRoutingModule { }
+export class CarsRoutingModule {
+}
