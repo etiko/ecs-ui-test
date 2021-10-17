8db9ac477ded85255dacb7eb3a80a5a5ec973564
Toyin Etiko <toyin.etiko@bolero.net>
2021-10-17 15:32:53 +0100

    setup car grid

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AgGridModule} from 'ag-grid-angular';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { CarsRoutingModule } from './cars-routing.module';
-import { CarComponent } from './components/car/car.component';
-import { CarGridComponent } from './components/car-grid/car-grid.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { ActionsRendererComponent } from './components/actions-renderer/actions-renderer.component';
+import {CarsRoutingModule} from './cars-routing.module';
+import {CarComponent} from './components/car/car.component';
+import {CarGridComponent} from './components/car-grid/car-grid.component';
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
+import {ActionsRendererComponent} from './components/actions-renderer/actions-renderer.component';
+
 
 
 @NgModule({
-  declarations: [CarComponent, CarGridComponent, LandingPageComponent, ActionsRendererComponent],
+  declarations: [
+    CarComponent,
+    CarGridComponent,
+    LandingPageComponent,
+  ],
   imports: [
     CommonModule,
-    CarsRoutingModule
+    CarsRoutingModule,
+    ReactiveFormsModule,
+    AgGridModule.withComponents([
+      ActionsRendererComponent
+    ]),
   ]
 })
-export class CarsModule { }
+export class CarsModule {
+}
--- src/app/cars/components/car-grid/car-grid.component.ts
+++ src/app/cars/components/car-grid/car-grid.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ColDef} from 'ag-grid-community';
+
+import {ActionsRendererComponent} from '../actions-renderer/actions-renderer.component';
 
 @Component({
   selector: 'app-car-grid',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-grid.component.scss']
 })
 export class CarGridComponent implements OnInit {
+  columnDefs: ColDef[] = [];
+  defaultColDef: any;
+  rowData: any;
+  frameworkComponents: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.columnDefs = [
+      {
+        headerName: 'Make',
+        field: 'make'
+      },
+      {
+        headerName: 'Model',
+        field: 'model'
+      },
+      {
+        headerName: 'Colour',
+        field: 'colour'
+      },
+      {
+        headerName: 'Year',
+        field: 'year'
+      },
+      {
+        headerName: '',
+        field: 'id',
+        width: 260,
+        cellRenderer: 'actionRenderer'
+      },
+    ];
+    this.frameworkComponents = {
+      actionRenderer: ActionsRendererComponent
+    };
   }
 
 }
